27c415d8e9aebafb271963236c9d66ce8ad5cec2
Kevin C. Tofel <kevin@kctofel.com>
2021-01-20 22:48:03 -0500

    First JSX

--- Week1/react-hello/src/index.js
+++ Week1/react-hello/src/index.js
@@ -3,12 +3,21 @@ import ReactDOM from 'react-dom';
 
 function HelloWorld() {
     return (
-        <div>Hello World!</div>
+        <div>
+            <Hello/> <World/>
+        </div>
     );
 }
 
-ReactDOM.render(
-    <HelloWorld/>,
-    document.querySelector('#root')
-);
+function Hello() {
+    return <span>Hello </span>;
+}
 
+function World() {
+    return <span>World!</span>;
+}
+
+ReactDOM.render(
+    <HelloWorld />,
+    document.querySelector('#root')
+)
